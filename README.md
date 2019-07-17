003eddfc53e985b9ccca5a209aef7403cc3e3a35
Erika Rasnick <erika.rasnick@gmail.com>
2019-07-17 12:51:21 -0400

    first commit; Rscript, readme draft

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# PEPR_greenspace
+# degauss/pepr_greenspace
+
+> DeGAUSS container that calculates average greenness within 500 m, 1500 m, and 2500 m buffers for PEPR multi-site study
+
+![](figs/evi_us.png)
+
+## Using
+
+DeGAUSS arguments specific to this container:
+
+- `file_name`: name of a CSV file in the current working directory with columns named `lat` and `lon`
+
+Example call (that will work with example file included in repository):
+
+```
+docker run --rm -v "$PWD":/tmp docker.pkg.github.com/cole-brokamp/pepr_greenspace:0.2 my_address_file_geocoded.csv
+```
+
+In the above example call, replace `geocoded_csv_file.csv` with the name of your geocoded csv file.
+
+Some progress messages will be printed and when complete, the program will save the output as the same name as the input file name, but with `pepr_greenspace` appended, e.g. `geocoded_csv_file_pepr_greenspace_cchmc.csv`
+
+## DeGAUSS Details
+
+For detailed documentation on DeGAUSS, including general usage and installation, please see the [DeGAUSS](https://github.com/cole-brokamp/DeGAUSS) README.
+
+This software is part of DeGAUSS and uses its same [license](https://github.com/cole-brokamp/DeGAUSS/blob/master/LICENSE.txt).
---
+++ get_greenspace.R
@@ -0,0 +1,86 @@
+#!/usr/local/bin/Rscript
+
+suppressPackageStartupMessages(library(argparser))
+p <- arg_parser('return greenspace for geocoded CSV file')
+p <- add_argument(p, 'file_name', help = 'name of geocoded csv file')
+args <- parse_args(p)
+
+suppressPackageStartupMessages(library(dplyr))
+suppressPackageStartupMessages(library(readr))
+suppressPackageStartupMessages(library(sp))    #required for raster package
+suppressPackageStartupMessages(library(raster))
+suppressPackageStartupMessages(library(sf))
+
+
+message('\nloading and projecting input file...')
+raw_data <- suppressMessages(read_csv(args$file_name))
+# raw_data <- supressMessages(read_csv("./my_address_file_geocoded.csv"))
+d_cc <- complete.cases(raw_data[ , c('lat','lon')])
+
+if (! all(d_cc)) {
+  message('\nWARNING: input file contains ', sum(!d_cc), ' rows with missing coordinates.')
+  message('\nWill return NA for all greenspace measures for these rows.')
+}
+
+d <- raw_data[d_cc, ]
+
+# store coords as separate numeric columns because
+# trans and back trans lead to rounding errors, making them unsuitable for merging
+d$old_lat <- d$lat
+d$old_lon <- d$lon
+
+d <- d %>%
+  st_as_sf(coords = c('lon', 'lat'), crs = 4326) %>%
+  st_transform(5072)
+
+### raster file is on GRAPPH Sharepoint site in __greenspace folder
+message('\nloading greenspace raster file...')
+evi_2018 <- raster::raster("evi_June_2018_5072.tif")
+
+message('\nfinding greenspace for each point...')
+message('\nusing 500 m buffer...')
+evi_buf_500 <- raster::extract(evi_2018,
+                               d,
+                               buffer = 500,
+                               fun = mean,
+                               na.rm = TRUE,
+                               small = TRUE,
+                               df = TRUE)
+
+message('\nusing 1500 m buffer...')
+evi_buf_1500 <- raster::extract(evi_2018,
+                               d,
+                               buffer = 1500,
+                               fun = mean,
+                               na.rm = TRUE,
+                               small = TRUE,
+                               df = TRUE)
+
+message('\nusing 2500 m buffer...')
+evi_buf_2500 <- raster::extract(evi_2018,
+                               d,
+                               buffer = 2500,
+                               fun = mean,
+                               na.rm = TRUE,
+                               small = TRUE,
+                               df = TRUE)
+
+d <- d %>% 
+  mutate(evi_500 = evi_buf_500$evi_June_2018_5072 * 0.0001,
+         evi_1500 = evi_buf_1500$evi_June_2018_5072 * 0.0001,
+         evi_2500 = evi_buf_2500$evi_June_2018_5072 * 0.0001) 
+
+# remove transformed coords and add back "old" coords
+d <- d %>%
+  st_set_geometry(NULL) %>%
+  rename(lon = old_lon,
+         lat = old_lat)
+
+# add back in original missing coords data for output file
+out_file <- suppressWarnings(bind_rows(raw_data[!d_cc, ], d))
+
+out_file_name <- paste0(tools::file_path_sans_ext(args$file_name), '_pepr_greenspace.csv')
+
+write_csv(out_file, out_file_name)
+
+message('\nFINISHED! output written to ', out_file_name)
