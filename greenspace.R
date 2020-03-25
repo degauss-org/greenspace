cd16bb3a02196c58b19fb32dcdfe5723d1ee8340
Erika <erika.rasnick@gmail.com>
2020-03-25 09:48:45 -0400

    clean up geomarker script to match template format

--- greenspace.R
+++ greenspace.R
@@ -1,41 +1,40 @@
 #!/usr/local/bin/Rscript
 
-suppressPackageStartupMessages(library(argparser))
-p <- arg_parser('return greenspace for geocoded CSV file')
-p <- add_argument(p, 'file_name', help = 'name of geocoded csv file')
-args <- parse_args(p)
-
 suppressPackageStartupMessages(library(dplyr))
 suppressPackageStartupMessages(library(readr))
 suppressPackageStartupMessages(library(sp))    #required for raster package
 suppressPackageStartupMessages(library(raster))
 suppressPackageStartupMessages(library(sf))
+suppressPackageStartupMessages(library(tidyr))
+suppressPackageStartupMessages(library(dplyr))
 
+doc <- '
+Usage:
+  greenspace.R <filename>
+'
 
-message('\nloading and projecting input file...')
-raw_data <- suppressMessages(read_csv(args$file_name))
-# raw_data <- supressMessages(read_csv("./my_address_file_geocoded.csv"))
-d_cc <- complete.cases(raw_data[ , c('lat','lon')])
+opt <- docopt::docopt(doc)
 
-if (! all(d_cc)) {
-  message('\nWARNING: input file contains ', sum(!d_cc), ' rows with missing coordinates.')
-  message('\nWill return NA for all greenspace measures for these rows.')
-}
+message('\reading input file...')
+raw_data <- readr::read_csv(opt$filename)
+# raw_data <- read_csv("./my_address_file_geocoded.csv")
 
-d <- raw_data[d_cc, ]
+## prepare data for calculations
+raw_data$.row <- seq_len(nrow(raw_data))
 
-# store coords as separate numeric columns because
-# trans and back trans lead to rounding errors, making them unsuitable for merging
-d$old_lat <- d$lat
-d$old_lon <- d$lon
-
-d <- d %>%
+d <-
+  raw_data %>%
+  dplyr::select(.row, lat, lon) %>%
+  na.omit() %>%
+  group_by(lat, lon) %>%
+  tidyr::nest(.rows = c(.row)) %>%
   st_as_sf(coords = c('lon', 'lat'), crs = 4326) %>%
   st_transform(5072)
 
 ### raster file is on GRAPPH Sharepoint site in __greenspace folder
 message('\nloading greenspace raster file...')
 evi_2018 <- raster::raster("/app/evi_June_2018_5072.tif")
+# evi_2018 <- raster::raster("./evi_June_2018_5072.tif")
 
 message('\nfinding greenspace for each point...')
 message('\nusing 500 m buffer...')
@@ -70,17 +69,14 @@ d <- d %>%
          evi_1500 = evi_buf_1500$evi_June_2018_5072 * 0.0001,
          evi_2500 = evi_buf_2500$evi_June_2018_5072 * 0.0001)
 
-# remove transformed coords and add back "old" coords
+
+## merge back on .row after unnesting .rows into .row
 d <- d %>%
-  st_set_geometry(NULL) %>%
-  rename(lon = old_lon,
-         lat = old_lat)
+  tidyr::unnest(cols = c(.rows)) %>%
+  st_drop_geometry()
 
-# add back in original missing coords data for output file
-out_file <- suppressWarnings(bind_rows(raw_data[!d_cc, ], d))
+out <- left_join(raw_data, d, by = '.row') %>% dplyr::select(-.row)
 
-out_file_name <- paste0(tools::file_path_sans_ext(args$file_name), '_pepr_greenspace.csv')
-
-write_csv(out_file, out_file_name)
-
+out_file_name <- paste0(tools::file_path_sans_ext(opt$filename), '_greenspace.csv')
+readr::write_csv(out, out_file_name)
 message('\nFINISHED! output written to ', out_file_name)
